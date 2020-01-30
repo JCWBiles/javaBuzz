2d028013c14bbfe05ee9da8e44bf728c74ae886a
JCWBiles <jcbiles@outlook.com>
2020-01-30 22:31:17 +0000

    added two more tests to say whether true or false for isDivisibleByFive - tests passing

--- jasmine-standalone-3.5.0/spec/javaBuzzSpec.js
+++ jasmine-standalone-3.5.0/spec/javaBuzzSpec.js
@@ -12,4 +12,16 @@ describe('Javabuzz', function () {
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
+  describe('knows when a number is', function() {
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+  describe('knows when a number is NOT', function() {
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+  });
 });

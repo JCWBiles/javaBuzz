962b3b360a9062e016ea1b71c1712a0e0583d5b1
JCWBiles <jcbiles@outlook.com>
2020-01-30 23:01:39 +0000

    code added to javaBuzz.js to be able to play isDivisibleByThree - test passes and 3 retruns 'Java'

--- jasmine-standalone-3.5.0/src/javaBuzz.js
+++ jasmine-standalone-3.5.0/src/javaBuzz.js
@@ -15,3 +15,9 @@ Javabuzz.prototype.isDivisibleByFifteen = function(number) {
 Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
   return (number % divisor === 0);
 };
+
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  }
+};
